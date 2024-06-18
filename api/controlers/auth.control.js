fcf6be9968a072550b6d12abe55a887b67d8919c
Vaishnavi Kanade <vaishnavikanade2003@gmail.com>
2024-06-18 13:39:53 +0530

    create middleware to handle possible errors

--- api/controlers/auth.control.js
+++ api/controlers/auth.control.js
@@ -1,23 +1,22 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
-  try {
-    let { username, email, password } = req.body;
-
+export const signup = async (req, res,next) => {
+    const { username, email, password } = req.body;
+    
     if (!username || !email || !password) {
-      return res.status(400).json({ message: 'Username, email, and password are required' });
+        return res.status(400).json({ message: 'Username, email, and password are required' });
     }
-
-     password = bcryptjs.hashSync(password, 10);
-
-    const newUser = new User({ username, email, password });
-
+    
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    
+    const newUser = new User({ username, email, password:hashedPassword});
+    
+    try {
     await newUser.save();
-
-    return res.status(201).json("User created Successfully");
+     res.send(201).json("User created Successfully");
   } catch (error) {
-    console.error('Error creating user:', error);
-    return res.status(500).json({ message: 'Error creating user', error });
+   next(error);
   }
 };
