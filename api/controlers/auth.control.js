ac120531200aa6881ae9d8cd5f3d0e267f2c457d
Vaishnavi Kanade <vaishnavikanade2003@gmail.com>
2024-06-19 14:02:57 +0530

    complete sign up page functionality

--- api/controlers/auth.control.js
+++ api/controlers/auth.control.js
@@ -2,7 +2,7 @@ import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res,next) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     
     if (!username || !email || !password) {
@@ -11,12 +11,14 @@ export const signup = async (req, res,next) => {
     
     const hashedPassword = bcryptjs.hashSync(password, 10);
     
-    const newUser = new User({ username, email, password:hashedPassword});
+    const newUser = new User({ username, email, password: hashedPassword });
     
     try {
-    await newUser.save();
-     res.send(201).json("User created Successfully");
-  } catch (error) {
-   next(error);
-  }
+        await newUser.save();
+        res.status(201).json({ message: 'User created Successfully' });
+    } catch (error) {
+        next(error);
+    }
 };
+
+export default signup;
